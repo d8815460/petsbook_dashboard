02345abf05a4ff6acbac27e7b6105f9560ea2bcd
Ian Chen <ian@Ians-MBP.ianchenhq.local>
2016-04-23 12:56:11 +0800

    Added material ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React, { PropTypes } from 'react'
 import Footer from './Footer'
+import AppBar from 'material-ui/lib/app-bar'
+import {Grid, Row, Col} from 'react-flexbox-grid'
+
+
 
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
@@ -7,7 +11,19 @@ import VisibleTodoList from '../containers/VisibleTodoList'
 const App = () => (
   <div>
     {/*<AddTodo />*/}
-    <VisibleTodoList />
+    <Grid>
+      <Row>
+        <AppBar
+          title="Petsbook 寵物飲水即時監控系統"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+      </Row>
+      <Row>
+        <Col xsOffset={6} xs={6} md={3}>
+          <VisibleTodoList />
+        </Col>
+      </Row>
+    </Grid>
     {/*<Footer />*/}
   </div>
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,45 @@
 var webpack = require('webpack')
 var path = require('path')
+var autoprefixer = require('autoprefixer')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 
 var BUILD_DIR = path.resolve(__dirname, 'public/dist')
 var APP_DIR = path.resolve(__dirname, 'src')
 
 var config = {
+  context: __dirname,                                   // input path
   entry: APP_DIR + '/index.js',
   devtool: 'source-map',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
   },
+  resolve: {
+    extensions: ['', '.jsx', '.scss', '.js', '.json'],  // along the way, subsequent file(s) to be consumed by webpack
+    modulesDirectories: [
+      'node_modules',
+      path.resolve(__dirname, './node_modules')
+    ]
+  },
   module: {
     loaders: [
       {
         test: /\.js/,
         include: APP_DIR,
+        exclude: /(node_modules)/,
         loader: 'babel'
+      },
+      {
+        test: /(\.scss|\.css)$/,
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap')
       }
     ]
-  }
+  },
+  postcss: [autoprefixer],
+  plugins: [
+    new ExtractTextPlugin('example.css', { allChunks: true }),
+  ]
 }
 
 module.exports = config
